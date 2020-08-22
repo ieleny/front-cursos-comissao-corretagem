f9eef96512b5e7d496dc445b1423e63edfd01d5a
ieleny <ielenyfilgueira@hotmail.com>
2020-08-22 15:55:46 -0300

    Logica da seta

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,39 @@
-$('#headingOne').collapse({
-    toggle: false
-})
+$('#collapseOne').on('shown.bs.collapse', function () {
+    $(".primeira-seta").css("background-image","url(../assets/externo/bootstrap-icons/arrow-down-short.svg)")
+});
+ 
+$('#collapseOne').on('hidden.bs.collapse', function () {
+    $(".primeira-seta").css("background-image","url(../assets/externo/bootstrap-icons/arrow-right-short.svg)")
+});
+
+$('#collapseTwo').on('shown.bs.collapse', function () {
+    $(".segunda-seta").css("background-image","url(../assets/externo/bootstrap-icons/arrow-down-short.svg)")
+});
+ 
+$('#collapseTwo').on('hidden.bs.collapse', function () {
+    $(".segunda-seta").css("background-image","url(../assets/externo/bootstrap-icons/arrow-right-short.svg)")
+});
+
+$('#collapseThree').on('shown.bs.collapse', function () {
+    $(".terceira-seta").css("background-image","url(../assets/externo/bootstrap-icons/arrow-down-short.svg)")
+});
+ 
+$('#collapseThree').on('hidden.bs.collapse', function () {
+    $(".terceira-seta").css("background-image","url(../assets/externo/bootstrap-icons/arrow-right-short.svg)")
+});
+
+$('#collapseFour').on('shown.bs.collapse', function () {
+    $(".quarta-seta").css("background-image","url(../assets/externo/bootstrap-icons/arrow-down-short.svg)")
+});
+ 
+$('#collapseFour').on('hidden.bs.collapse', function () {
+    $(".quarta-seta").css("background-image","url(../assets/externo/bootstrap-icons/arrow-right-short.svg)")
+});
+
+$('#collapseFive').on('shown.bs.collapse', function () {
+    $(".quinta-seta").css("background-image","url(../assets/externo/bootstrap-icons/arrow-down-short.svg)")
+});
+ 
+$('#collapseFive').on('hidden.bs.collapse', function () {
+    $(".quinta-seta").css("background-image","url(../assets/externo/bootstrap-icons/arrow-right-short.svg)")
+});
